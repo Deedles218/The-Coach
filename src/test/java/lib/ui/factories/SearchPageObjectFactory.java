b9b631d433e457051dff99a8f1a0914b63b462f6
DaryaSamorukova <33025134+DaryaSamorukova@users.noreply.github.com>
2021-01-28 23:56:59 +0400

    Refactoring project Factories

--- src/test/java/lib/ui/factories/SearchPageObjectFactory.java
+++ src/test/java/lib/ui/factories/SearchPageObjectFactory.java
@@ -1,21 +1,15 @@
 package lib.ui.factories;
 import lib.Platform;
-import lib.ui.SearchPageObject;
-import lib.ui.android.AndroidSearchPageObject;
 import lib.ui.ios.iOSSearchPageObject;
-import lib.ui.mobile_web.MWSearchPageObject;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class SearchPageObjectFactory
 {
-    public static SearchPageObject get(RemoteWebDriver driver)
+    public static iOSSearchPageObject get(RemoteWebDriver driver)
     {
-        if (Platform.getInstance().isAndroid()) {
-            return new AndroidSearchPageObject(driver);
-        }else if (Platform.getInstance().isIOS()){
+        if(Platform.getInstance().isIOS()){
             return new iOSSearchPageObject(driver);
-        }else {
-        return new MWSearchPageObject(driver);
         }
+        return null;
     }
 }
