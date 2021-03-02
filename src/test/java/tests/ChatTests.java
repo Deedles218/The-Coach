6935eb182d4ccaca73d72bf35f22eae37d866b4e
DaryaSamorukova <33025134+DaryaSamorukova@users.noreply.github.com>
2021-03-02 22:22:40 +0400

    Add tags in ProfileTests & ChatTests

--- src/test/java/tests/ChatTests.java
+++ src/test/java/tests/ChatTests.java
@@ -1,14 +1,20 @@
 package tests;
 
-import io.qameta.allure.Epic;
+import io.qameta.allure.*;
+import io.qameta.allure.junit4.DisplayName;
 import lib.CoreTestCase;
 import lib.ui.SearchPageObject;
 import lib.ui.factories.SearchPageObjectFactory;
 import org.junit.Test;
-
+@Epic(value = "Chat")
 public class ChatTests extends CoreTestCase {
 
     @Test
+    @Features(value ={@Feature(value = "ChatBot"),@Feature(value = "Chat")} )
+    @DisplayName("Интент /debug_image")
+    @Description("Тест отправляет в чате дебажное сообщение и проверяет что пришла картинка")
+    @Step("Start test testChatInput1")
+    @Severity(value = SeverityLevel.NORMAL)
     public void testChatInput1() throws InterruptedException
     {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -20,6 +26,12 @@ public class ChatTests extends CoreTestCase {
         //дописать тап на картинку или проверку что картинка пришла
         SearchPageObject.tapOnImage();
     }
+    @Test
+    @Features(value ={@Feature(value = "ChatBot"),@Feature(value = "Chat")} )
+    @DisplayName("Интент /debug_gif")
+    @Description("Тест отправляет в чате дебажное сообщение и проверяет что пришла гифка")
+    @Step("Start test testChatInput2")
+    @Severity(value = SeverityLevel.NORMAL)
     public void testChatInput2() throws InterruptedException
     {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -31,6 +43,12 @@ public class ChatTests extends CoreTestCase {
         //дописать тап на гифку или проверку что гиф пришла
         SearchPageObject.tapOnGif();
     }
+    @Test
+    @Features(value ={@Feature(value = "ChatBot"),@Feature(value = "Chat")} )
+    @DisplayName("Интент /debug_pdf_test")
+    @Description("Тест отправляет в чате дебажное сообщение и проверяет что пришел пдф файл")
+    @Step("Start test testChatInput3")
+    @Severity(value = SeverityLevel.NORMAL)
     public void testChatInput3() throws InterruptedException
     {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -41,6 +59,13 @@ public class ChatTests extends CoreTestCase {
         Thread.sleep(7000);
         //дописать тап на картинку или проверку что картинка пришла
     }
+
+    @Test
+    @Features(value ={@Feature(value = "ChatBot"),@Feature(value = "Chat")} )
+    @DisplayName("Интент /debug_selector")
+    @Description("Тест отправляет в чате дебажное сообщение и получает селекторы")
+    @Step("Start test testChatInput4")
+    @Severity(value = SeverityLevel.NORMAL)
     public void testChatInput4() throws InterruptedException
     {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -51,6 +76,13 @@ public class ChatTests extends CoreTestCase {
         Thread.sleep(7000);
         //дописать тап на картинку или проверку что картинка пришла
     }
+
+    @Test
+    @Features(value ={@Feature(value = "ChatBot"),@Feature(value = "Chat")} )
+    @DisplayName("Интент /debug_show_video")
+    @Description("Тест отправляет в чате дебажное сообщение и получает видео")
+    @Step("Start test testChatInput5")
+    @Severity(value = SeverityLevel.NORMAL)
     public void testChatInput5() throws InterruptedException
     {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -61,6 +93,13 @@ public class ChatTests extends CoreTestCase {
         Thread.sleep(7000);
         //дописать тап на картинку или проверку что картинка пришла
     }
+
+    @Test
+    @Features(value ={@Feature(value = "ChatBot"),@Feature(value = "Chat")} )
+    @DisplayName("Интент /debug_daily_plan")
+    @Description("Тест отправляет в чате дебажное сообщение и получает карточку с планом на день")
+    @Step("Start test testChatInput6")
+    @Severity(value = SeverityLevel.NORMAL)
     public void testChatInput6() throws InterruptedException
     {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -71,6 +110,14 @@ public class ChatTests extends CoreTestCase {
         Thread.sleep(7000);
         //дописать тап на картинку или проверку что картинка пришла
     }
+
+    @Test
+    @Features(value ={@Feature(value = "ChatBot"),@Feature(value = "Chat")} )
+    @DisplayName("Интент /debug_link")
+    @Description("Тест отправляет в чате дебажное сообщение и получает сообщение с ссылкой")
+    @Step("Start test testChatInput7")
+    @Severity(value = SeverityLevel.NORMAL)
+
     public void testChatInput7() throws InterruptedException
     {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -81,6 +128,13 @@ public class ChatTests extends CoreTestCase {
         Thread.sleep(7000);
         //дописать тап на картинку или проверку что картинка пришла
     }
+
+    @Test
+    @Features(value ={@Feature(value = "ChatBot"),@Feature(value = "Chat")} )
+    @DisplayName("Интент //debug_cards")
+    @Description("Тест отправляет в чате дебажное сообщение и получает карточку с оранжевой кнопкой")
+    @Step("Start test testChatInput8")
+    @Severity(value = SeverityLevel.NORMAL)
     public void testChatInput8() throws InterruptedException
     {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -91,6 +145,13 @@ public class ChatTests extends CoreTestCase {
         Thread.sleep(7000);
         //дописать тап на картинку или проверку что картинка пришла
     }
+    @Test
+    @Features(value ={@Feature(value = "ChatBot"),@Feature(value = "Chat")} )
+    @DisplayName("Интент /debug_questionnaire")
+    @Description("Тест отправляет в чате дебажное сообщение и получает карточку daily survey")
+    @Step("Start test testChatInput9")
+    @Severity(value = SeverityLevel.NORMAL)
+
     public void testChatInput9() throws InterruptedException
     {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -101,6 +162,12 @@ public class ChatTests extends CoreTestCase {
         Thread.sleep(7000);
         //дописать тап на картинку или проверку что картинка пришла
     }
+    @Test
+    @Features(value ={@Feature(value = "ChatBot"),@Feature(value = "Chat")} )
+    @DisplayName("Интент /debug_controls_dimension_picker_height")
+    @Description("Тест отправляет в чате дебажное сообщение и получает пикер роста")
+    @Step("Start test testChatInput10")
+    @Severity(value = SeverityLevel.NORMAL)
     public void testChatInput10() throws InterruptedException
     {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -111,6 +178,12 @@ public class ChatTests extends CoreTestCase {
         Thread.sleep(7000);
         //дописать тап на картинку или проверку что картинка пришла
     }
+    @Test
+    @Features(value ={@Feature(value = "ChatBot"),@Feature(value = "Chat")} )
+    @DisplayName("Интент /debug_controls_dimension_picker_weight")
+    @Description("Тест отправляет в чате дебажное сообщение и получает пикер веса")
+    @Step("Start test testChatInput11")
+    @Severity(value = SeverityLevel.NORMAL)
     public void testChatInput11() throws InterruptedException
     {
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
--- src/test/java/tests/ProfileTests.java
+++ src/test/java/tests/ProfileTests.java
@@ -1,14 +1,22 @@
 package tests;
 
+import io.qameta.allure.*;
+import io.qameta.allure.junit4.DisplayName;
 import lib.CoreTestCase;
 import lib.ui.ProfilePageObject;
 import lib.ui.SearchPageObject;
 import lib.ui.factories.ProfilePageObjectFactory;
 import lib.ui.factories.SearchPageObjectFactory;
 import org.junit.Test;
-
+@Epic(value = "Profile")
 public class ProfileTests extends CoreTestCase {
     @Test
+
+    @Features(value ={@Feature(value = "Program"),@Feature(value = "Tracks")} )
+    @DisplayName("Открыть настройки профиля")
+    @Description("Тест открывает экран с данными пользователя")
+    @Step("Start test testOpenProfile")
+    @Severity(value = SeverityLevel.NORMAL)
     public void testOpenProfile(){
         ProfilePageObject ProfilePageObject = ProfilePageObjectFactory.get(driver);
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -16,6 +24,11 @@ public class ProfileTests extends CoreTestCase {
         ProfilePageObject.tapOnProfileMenu();
     }
     @Test
+    @Features(value ={@Feature(value = "Program"),@Feature(value = "Tracks")} )
+    @DisplayName("Данные на экране инфы о пользователе")
+    @Description("Проверяем что на экране есть terms privacy и линк отправки фидбэка")
+    @Step("Start test testAssertUserData")
+    @Severity(value = SeverityLevel.BLOCKER)
     public void testAssertUserData(){
         ProfilePageObject ProfilePageObject = ProfilePageObjectFactory.get(driver);
         SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
@@ -26,6 +39,11 @@ public class ProfileTests extends CoreTestCase {
         ProfilePageObject.assertSendFeedback();
     }
     @Test
+    @Features(value ={@Feature(value = "Program"),@Feature(value = "Tracks")} )
+    @DisplayName("Трэк нутришн фастинг")
+    @Description("Открываем фастинг включаем его запускаем таймер")
+    @Step("Start test testOpenNutrition")
+    @Severity(value = SeverityLevel.BLOCKER)
     public  void testOpenNutrition()throws InterruptedException{
         ProfilePageObject ProfilePageObject = ProfilePageObjectFactory.get(driver);
         ProfilePageObject.tapOnProgramTab();
