0708d7fe2c954e559ccc0e6b00bb4f535130447b
DaryaSamorukova <33025134+DaryaSamorukova@users.noreply.github.com>
2021-03-02 23:51:23 +0400

    Add AutoSuite

---
+++ src/test/java/suites/AutoSuite.java
@@ -0,0 +1,16 @@
+package suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import tests.ChatTests;
+import tests.ProfileTests;
+import tests.SearchTests;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        SearchTests.class,
+        ProfileTests.class,
+        ChatTests.class
+})
+public class AutoSuite {
+}
